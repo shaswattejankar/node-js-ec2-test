1c751f2c5587864a4dd72593ef25dc7555461a79
shaswattejankar <shaswattejankar+github@gmail.com>
2024-01-05 18:14:23 +0530

    trying express server

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-var http = require('http');
+// var http = require('http');
 
-//create a server object:
-http.createServer(function (req, res) {
-  res.write('<html><body><h1>Hello World!</h1></body></html>'); //write a response to the client
-  res.end(); //end the response
-}).listen(80); //the server object listens on port 8000
+// //create a server object:
+// http.createServer(function (req, res) {
+//   res.write('<html><body><h1>Hello World!</h1></body></html>'); //write a response to the client
+//   res.end(); //end the response
+// }).listen(80); //the server object listens on port 8000
 
-// const express = require('express');
-// const app = express();
+const express = require('express');
+const app = express();
 
-// app.get('/',function(req,res){
-//     res.send("<html><body><h1>Hello World!</h1></body></html>");
-// })
+app.get('/',function(req,res){
+    res.send("<html><body><h1>Hello World!</h1></body></html>");
+})
 
-// const server = app.listen(80, function(){
-//     console.log("Node app server started on port 80...")
-// })
+const server = app.listen(80, function(){
+    console.log("Node app server started on port 80...")
+})
