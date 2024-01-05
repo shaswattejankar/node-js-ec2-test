3a336544f5d684157a0ae260ef2f977cde79baf1
shaswattejankar <shaswattejankar+github@gmail.com>
2024-01-05 17:55:10 +0530

    adding start script and creating http server

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const express = require('express');
-const app = express();
+var http = require('http');
 
-app.get('/',function(req,res){
-    res.send("<html><body><h1>Hello World!</h1></body></html>");
-})
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('<html><body><h1>Hello World!</h1></body></html>'); //write a response to the client
+  res.end(); //end the response
+}).listen(8000); //the server object listens on port 8000
 
-const server = app.listen(5000, function(){
-    console.log("Node app server started...")
-})
+// const express = require('express');
+// const app = express();
+
+// app.get('/',function(req,res){
+//     res.send("<html><body><h1>Hello World!</h1></body></html>");
+// })
+
+// const server = app.listen(5000, function(){
+//     console.log("Node app server started...")
+// })
